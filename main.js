6c9fd75f94caf20b5bcaca1d494041be14b398c9
MeraryMC <m_martinez_cobian@berkeley.edu>
2019-08-06 11:24:08 -0700

    reviewed variable declaration and variable typing

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ exports.__esModule = true;
 var message = "Hello World";
 console.log(message);
 //On the command line, use the command “tsc /*file name*/ --watch” to have the file recompile whenever you save
+var x = 10;
+var y = 20;
+var sum;
+var title = "Codevolution";
+var isBeginner = true;
+var total = 0;
+var name = "Merary";
--- main.ts
+++ main.ts
@@ -3,3 +3,14 @@ let message = "Hello World";
 console.log(message);
 
 //On the command line, use the command “tsc /*file name*/ --watch” to have the file recompile whenever you save
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = "Codevolution";
+
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = "Merary"
+
